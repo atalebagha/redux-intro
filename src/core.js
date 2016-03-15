10ecd44cfe7102c48d5def2e523d7b24478e945a
Abdul R. Taleb-Agha <abdul-rahman.taleb-agha@one.verizon.com>
2016-03-14 17:28:54 -0700

    setting up socket.io server

--- src/core.js
+++ src/core.js
@@ -1,23 +1,25 @@
 import {List, Map} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
 function getWinners(vote) {
   if (!vote) return [];
-  const [a,b] = vote.get('pair');
+  const [a, b] = vote.get('pair');
   const aVotes = vote.getIn(['tally', a], 0);
   const bVotes = vote.getIn(['tally', b], 0);
   if (aVotes > bVotes) return [a];
   else if (aVotes < bVotes) return [b];
-  else return [a,b]
+  else return [a, b];
 }
 
 export function next(state) {
   const entries = state.get('entries').concat(getWinners(state.get('vote')));
   if (entries.size === 1) {
-    return state.remove('vote').remove('entries').set('winner', entries.first())
+    return state.remove('vote').remove('entries').set('winner', entries.first());
   } else {
     return state.merge({
       vote: Map({pair: entries.take(2)}),
@@ -28,7 +30,7 @@ export function next(state) {
 
 export function vote(state, entry) {
   return state.updateIn(
-    ['vote', 'tally', entry],
+    ['tally', entry],
     0,
     tally => tally+1
     );
